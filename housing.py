f743f26d733ef50e51fc2a50886ab1bbe1cfa56e
jeetbuz <jitesh.ice@gmail.com>
2017-10-16 16:31:02 +0530

    dummy
    dummy housing.py

---
+++ housing.py
@@ -0,0 +1,5 @@
+“housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
+housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["population_per_household"]=housing["population"]/housing["households"]”
+
+Excerpt From: Aurélien Géron. “Hands-On Machine Learning with Scikit-Learn and TensorFlow: Concepts, Tools, and Techniques to Build Intelligent Systems.” iBooks. 
